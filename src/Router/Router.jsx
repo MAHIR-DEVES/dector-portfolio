c49c44e77df3f36d91e89a5cb06c6d3ca62f2d3a
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-09-19 21:45:18 +0600

    add whatapp call icon

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router';
 import Root from '../Layout/Root';
 import Home from '../Pages/Home/Home';
-import About from '../Pages/About/About';
 
 export const router = createBrowserRouter([
   {
@@ -11,10 +10,6 @@ export const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
-      },
-      {
-        path: '/about',
-        Component: About,
       },
     ],
   },
